fa01f169354e50e804c3ae065301359750cae71c
Josh <j.j.h.lindner@gmail.com>
2021-03-15 20:17:50 -0700

    This script take the rsync command for backing up large directories and incorporates multiprocessing.

---
+++ dailysync.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from multiprocessing import Pool
+import subprocess
+import os
+
+
+
+def run(directory_list):
+
+        src = directory_list
+        dest = os.getcwd()+"/data/prod_backup/"
+        subprocess.call(["rsync", "-arq", src, dest])
+
+if __name__ == "__main__":
+        src = os.getcwd()+"/data/prod/"
+
+        directory_list = []
+        for i in os.listdir(src):
+                directory_list.append(os.path.join(src, i))
+        p = Pool(len(directory_list))
+        p.map(run, directory_list)
